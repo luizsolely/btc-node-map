e83458a59b268467a15db9ce5c9d8c9e15f19634
luizsolely <solelydev1@gmail.com>
2025-06-20 16:29:10 -0300

    chore: setting up configs and base-packages

---
+++ src/main/java/com/luizsolely/btcnodemap/BtcnodemapApplication.java
@@ -0,0 +1,13 @@
+package com.luizsolely.btcnodemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtcnodemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtcnodemapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizsolely/btcnodemap/BtcnodemapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizsolely.btcnodemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtcnodemapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
