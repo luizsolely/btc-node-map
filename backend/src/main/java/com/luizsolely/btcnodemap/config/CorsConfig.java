745c70188e13ba493c155267bbf2d88f47bdd7a7
luizsolely <solelydev1@gmail.com>
2025-06-28 15:29:06 -0300

    updates for deploy

--- backend/src/main/java/com/luizsolely/btcnodemap/config/CorsConfig.java
+++ backend/src/main/java/com/luizsolely/btcnodemap/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.luizsolely.btcnodemap.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${CORS_ALLOWED_ORIGINS}")
+    private String allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // porta do frontend
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
